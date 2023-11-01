155a132e477f6851e775fc9327e6805d9d3157ad
waizbart <guilhermewaizbart@gmail.com>
2023-11-01 19:34:09 -0300

    fix: felipinho

--- src/Interface/Tabs/Clientes/Tabs/Buscar.java
+++ src/Interface/Tabs/Clientes/Tabs/Buscar.java
@@ -2,12 +2,19 @@ package Interface.Tabs.Clientes.Tabs;
 import javax.swing.*;
 
 import Managers.ClientManager;
+import Models.Client.Client;
 
 public class Buscar extends JPanel {
     private static ClientManager clientManager = ClientManager.getInstance();
 
     public Buscar() {
-        add(new JLabel("Buscar"));
+        String clients = "";
+
+        for (Client client : clientManager.getClientsPf()) {
+            clients += client.toString() + "\n";
+        }
+
+        add(new JLabel(clients));
     }
 }
 
--- src/Managers/ClientManager.java
+++ src/Managers/ClientManager.java
@@ -1,7 +1,7 @@
 package Managers;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Vector;
 
 import Models.Client.Address;
@@ -11,8 +11,8 @@ import Models.Client.ClientPJ;
 
 public class ClientManager {
     private static ClientManager instance = null;
-    private Vector<ClientPF> clientsPf;
-    private Vector<ClientPJ> clientsPj;
+    private ArrayList<ClientPF> clientsPf = new ArrayList<ClientPF>();
+    private ArrayList<ClientPJ> clientsPj = new ArrayList<ClientPJ>();
 
     private ClientManager() {
         FileManager fileManager = new FileManager();
@@ -21,8 +21,9 @@ public class ClientManager {
         Vector<String[]> clientsData = fileManager.readRows("baseDados/clients.txt");
 
         for (String[] clientData : clientsData) {
+
             try {
-                if (clientData[0] == "PJ") {
+                if (clientData[0].equals("PJ")) {
                     Address address = new Address(clientData[6], clientData[7], clientData[8], clientData[9],
                             clientData[10], clientData[11]);
                     clientsPj.add(new ClientPJ(clientData[1], clientData[2], Integer.parseInt(clientData[3]),
@@ -30,7 +31,7 @@ public class ClientManager {
                 } else {
                     Address Address = new Address(clientData[5], clientData[6], clientData[7], clientData[8],
                             clientData[9], clientData[10]);
-                    clientsPf.add(new ClientPF(clientData[1], clientData[2], Integer.parseInt(,
+                    clientsPf.add(new ClientPF(clientData[1], Integer.parseInt(clientData[2]), clientData[3],
                             format.parse(clientData[4]), Address));
                 }
             } catch (Exception e) {
@@ -63,5 +64,16 @@ public class ClientManager {
 
     public void delete() {
         System.out.println("ClientManager.delete()");
+    }
+
+    public ArrayList<ClientPF> getClientsPf() {
+        return clientsPf;
+    }
+
+    public ArrayList<Client> getClients(){
+        ArrayList<Client> clients = new ArrayList<Client>();
+        clients.addAll(clientsPf);
+        clients.addAll(clientsPj);
+        return clients;
     }
 }
